833602cc98f0daae3e7fca5a6b5fb7825382bf41
nmy6452 <85818940+nmy6452@users.noreply.github.com>
2022-04-27 12:37:23 +0900

    FEAT: 5층 페이징 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            if (camera.transform.position.x <= 0 && Input.GetAxisRaw("Horizontal") == -1)
+                camera.transform.position = new Vector3(75, 0, -10); 
+            else if (camera.transform.position.x >= 75 && Input.GetAxisRaw("Horizontal") == 1)
+                camera.transform.position = new Vector3(0, 0, -10);
+            else
+                camera.transform.position += new Vector3(25, 0, 0) * Input.GetAxisRaw("Horizontal");
+        }
         
     }
 
@@ -26,4 +36,10 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("prologue");
     }
+
+    public void new_gamestart()
+    {
+        SceneManager.LoadScene("stage1_5f");
+    }
+
 }

107adbcdc79f27b27745064e8172a05c8d19d7cc
nmy6452 <nmy6452@naver.com>
2022-09-06 09:58:25 +0900

    FEAT: make bells

--- Assets/Script/navigation_puzzle.cs
+++ Assets/Script/navigation_puzzle.cs
@@ -1,47 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class navigation_puzzle : MonoBehaviour
 {
     public RectTransform Startpoint,Endpoint;
+    public GameObject PuzzleManager;
     public GameObject cross;
-    // Start is called before the first frame update
+    string[] password = {"up", "right", "right", "down", "right", "right", "up", "up", "up", "right", "right",  "down", "right", "right", "up", "right" };
+    public List<string> password_input;
+    // Start is called before the first frame "up"date
     void Start()
     {
+        PuzzleManager = GameObject.Find("PuzzleManager");
         Startpoint = GameObject.Find("StartPoint").GetComponent<RectTransform>();
         Endpoint = GameObject.Find("EndPoint").GetComponent<RectTransform>();
         cross = GameObject.Find("Cross");
         cross.transform.SetParent(Startpoint, false);
-        cross.transform.localPosition = new Vector3(50, -50, 0);
+        cross.transform.localPosition = new Vector3(0, 0, 0);
     }
 
-    public void Cross_move(int a)
+    public void Cross_init()
     {
-        switch (a)
+        cross.transform.SetParent(Startpoint, false);
+        password_input.Clear();
+
+        for (int i = 0; i < 40; i++)
         {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
+            transform.GetChild(i).GetComponent<Image>().color = new Color(255, 255, 225, 225);
+            transform.GetChild(i).gameObject.tag = "road";
+        }
+    }
+
+    public void Croos_tile(int a)
+    {
+        //레이케스트 초기화
+        float MaxDistance = 50f;
+        Vector3 cross_pos = cross.transform.parent.transform.position;
+        RaycastHit2D hit;
+
+
+        //레이케스트 발사
+        if (a == 1)
+        {
+            Debug.DrawRay(cross_pos += new Vector3(-50, 0, 0), Vector3.left * MaxDistance, Color.blue, 1f);
+            hit = Physics2D.Raycast(cross_pos += new Vector3(-50, 0, 0), Vector3.left, MaxDistance);
+            password_input.Add("left");
+        }
+        else if (a == 2)
+        {
+            Debug.DrawRay(cross_pos += new Vector3(50, 0, 0), Vector3.right * MaxDistance, Color.blue, 1f);
+            hit = Physics2D.Raycast(cross_pos += new Vector3(50, 0, 0), Vector3.right, MaxDistance);
+            password_input.Add("right");
+        }
+        else if (a == 3)
+        {
+            Debug.DrawRay(cross_pos += new Vector3(0, 50, 0), Vector3.up * MaxDistance, Color.blue, 1f);
+            hit = Physics2D.Raycast(cross_pos += new Vector3(0, 50, 0), Vector3.up, MaxDistance);
+            password_input.Add("up");
+        }
+        else
+        {
+            Debug.DrawRay(cross_pos += new Vector3(0, -50, 0), Vector3.down * MaxDistance, Color.blue, 1f);
+            hit = Physics2D.Raycast(cross_pos += new Vector3(0, -50, 0), Vector3.down, MaxDistance);
+            password_input.Add("down");
+        }
+
+        //레이케스트가 맞을 경우
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "road")
+            {
+                cross.transform.parent.tag = "stop";
+                cross.transform.parent.GetComponent<Image>().color = Color.gray;
+                cross.transform.SetParent(hit.transform, false);
+            }
+            if (hit.collider.name == "EndPoint" && Croos_tile())
+            {
+                Debug.Log("정답입니다.");
+            }
         }
     }
 
     bool Croos_tile()
     {
-        cross.transform.parent
-        return false;
+        for (int i = 0; i < password_input.Count; i++)
+        {
+            if (password_input[i] != password[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     void Update()
     {
-        //if (Input.GetKeyDown("a"))
-        //{
-        //    gameObject.GetComponent<RectTransform>().position += new Vector3(gameObject.GetComponent<RectTransform>().position.x + 100, 0,0);
-        //}
+        if (Input.GetKeyDown("a"))
+        {
+            Croos_tile(1);
+        }
     }
 }
